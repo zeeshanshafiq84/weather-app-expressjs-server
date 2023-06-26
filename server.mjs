4f7ac57b8c0f8dd9ab3fc06b1ef876b66f688142
zeeshan <zeeshanshafiq84>
2023-06-26 07:07:17 -0700

    done

--- server.mjs
+++ server.mjs
@@ -1,19 +1,30 @@
 import express from 'express';
+import cors from 'cors';  
+import path from 'path';
+const __dirname = path.resolve()
 
 const app = express()
+app.use(cors())
 
-
-app.get('/', (req, res) => {
-    console.log('Hello World!', new Date());
-    res.send('Hello World!' + new Date());
+app.get('/profile',(req, res) =>{
+  console.log('this is profile!', new Date());
+  res.send('this is profile' + new Date());
 })
 
-app.get('/profile', (req, res) => {
-  console.log('This is profile!', new Date());
-  res.send('This is profile!' + new Date());
+app.get('/weather/:cityName', (req, res) => {
+  console.log('this is profile!', new Date());
+
+  console.log("req.params.cityName: ", req.params.cityName)
+
+    let cityName = document.querySelector("#cityName").value;
+
+    
+
 })
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
-})
+})  
